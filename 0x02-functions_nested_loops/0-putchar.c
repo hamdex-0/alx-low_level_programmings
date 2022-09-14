7312fba21c7ca3c09c90cb4a64d273218696b308
hamdi_abdulaziz <hamdikore@gmail.com>
2022-09-14 01:50:59 -0700

    hmm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point function
+ *
+ * Description: Prints putchar using a library followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
